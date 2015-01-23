8e6fc8b1b8a5cf5ae295127778220d5da7584ac1
Jasnan TP <tp.jasnan@hotmail.com>
2015-01-23 19:20:41 +0530

    File archieve Code refactored

--- server/api/thing/index.js
+++ server/api/thing/index.js
@@ -6,6 +6,6 @@ var controller = require('./thing.controller');
 var router = express.Router();
 
 // router.get('/', controller.index);
-router.post('/', controller.getScript);
+router.post('/', controller.downloadGen, controller.getScript);
 
 module.exports = router;
--- server/api/thing/thing.controller.js
+++ server/api/thing/thing.controller.js
@@ -7,113 +7,124 @@ var outputDir = __dirname  + '/downloads/' + 'meansible_';
 var custDir = 'meansible_';
 var async = require('async');
 var AdmZip = require('adm-zip');
+var JSZip = require('jszip');
 var path = require('path');
 
-function ensureExists(path, mask, cb) {
-    if (typeof mask == 'function') { // allow the `mask` parameter to be optional
-        cb = mask;
-        mask = 484;
-    }
-    fs.mkdir(path, mask, function(err) {
-        if (err) {
-           cb(err,null); // something went wrong
-        } else {
-          // console.log(path);
-          cb(null,path);
-        }
-        // successfully created folder
-    });
-}
+// function ensureExists(path, mask, cb) {
+//     if (typeof mask == 'function') { // allow the `mask` parameter to be optional
+//         cb = mask;
+//         mask = 484;
+//     }
+//     fs.mkdir(path, mask, function(err) {
+//         if (err) {
+//            cb(err,null); // something went wrong
+//         } else {
+//           // console.log(path);
+//           cb(null,path);
+//         }
+//         // successfully created folder
+//     });
+// }
 
-// Generate and Send Scripts
-exports.getScript = function(req, res) {
+
+exports.downloadGen = function(req, res, next) {
   var hostname = req.body.hostname || 'default';
   var outputDir = path.normalize(__dirname + '/../../../client/assets/downloads/meansible_' + hostname)
   // outputDir = __dirname  + '/downloads/' + 'meansible_' + hostname;
   custDir = 'meansible_' + hostname;
 
   async.waterfall([
-    function(cb){ 
-      ensureExists(outputDir, 484, function callback(err,path) {
-        if (err) {
-            if (err.code == 'EEXIST') {
-                var rndNow = Date.now();
-                outputDir = outputDir + '-' + rndNow;
-                custDir = 'meansible_' + hostname + '-' + rndNow;
-                ensureExists(outputDir, 484, callback); // Call again
-                // return console.log("A Folder with same name already exists");
-            } else {
-                return console.log(err);
-            };
-        }
-
-        path = path || outputDir;
-        cb(null,path);
-      });
-    },
-
-    function(path,cb){
-      // console.log("hhhhhhhhhhhh" + "  " + path)
+    function(cb){
       var jsondata = req.body;
       //VagrantFile generation
       fs.readFile(__dirname + "/pages/vagrant.ejs", 'utf-8', function(err, data) {      
-        // console.log(data) 
-        var vagrant_out = ejs.render(data, {data:jsondata});
-        var vagrantFile = path + '/Vagrantfile';
-        fs.outputFile(vagrantFile, vagrant_out, function(err) {
-          console.log(err) 
-        });  
+        var vagrantFileBuff = ejs.render(data, {data:jsondata});
+        var payload = {};
+        payload.vagrantFile = vagrantFileBuff;
+        cb(null,payload);
       })
-
-      //Playbook generation
-      fs.readFile(__dirname + "/pages/playbook.ejs", 'utf-8', function(err, data) {
-        var playbook_out = ejs.render(data, {data:jsondata});
-        var playbookFile = path + '/playbook.yml';
-        fs.outputFile(playbookFile, playbook_out, function(err) {
-          console.log(err) // => null
-        });
+    },
+    
+    function(payload,cb){
+      var jsondata = req.body;
+      //VagrantFile generation
+      fs.readFile(__dirname + "/pages/playbook.ejs", 'utf-8', function(err, data) {      
+        var playbookFileBuff = ejs.render(data, {data:jsondata});        
+        payload.playbookFile = playbookFileBuff;
+        cb(null,payload);
       })
+    },  
 
+    function(payload,cb){
+      var jsondata = req.body;
+      payload.roles = {};
       //Create Role Files
       jsondata.roles.forEach(function(role){
         fs.readFile(__dirname + "/pages/roles/" + role + "/tasks/main.ejs", 'utf-8', function(err, data) {
           // console.log(data);
           var role_out = ejs.render(data, {data:jsondata});
-          var roleFile = path + '/roles/'+ role + '/tasks/main.yml';
-          fs.outputFile(roleFile, role_out, function(err) {
-            console.log(err) // => null
-          });
+          payload.roles[role] = role_out;          
         })
-      });   
+      }); 
 
       setTimeout(function(){
-        cb(null, path, 'generated');
+        cb(null,payload);
       }, 1000);  
       
     },
+    function(payload,cb){
+      // var zip = new AdmZip();
+      // zip.addLocalFolder(path1);
+      // zip.writeZip(path1 + ".zip");      
+      // cb(null,"/assets/downloads/" + path.basename(path1 + ".zip"));
 
-    function(path1,arg1,cb){
-      // console.log("The path is : ----" ,path1,"-------")
-      var zip = new AdmZip();
-      zip.addLocalFolder(path1);
-      zip.writeZip(path1 + ".zip");      
-      cb(null,"/assets/downloads/" + path.basename(path1 + ".zip"));
+      var zip = new JSZip();
+      var hostname = req.body.hostname || 'default';
+      var outputFile = path.normalize(__dirname + '/../../../client/assets/downloads/meansible_' + hostname)
+
+      if(payload.vagrantFile) {
+        zip.file("Vagrantfile", payload.vagrantFile);
+      }
+
+      if(payload.playbookFile) {
+        zip.file("playbook.yml", payload.playbookFile);
+      }    
+
+      _.forIn(payload.roles, function(value, key) {
+        zip.file("roles/"+key+"/tasks/main.yml", value);
+      });
+
+      // var content = zip.generate({type:"blob"});
+      var buffer = zip.generate({type:"nodebuffer"});
+
+      fs.exists(outputFile + ".zip", function callback(exists) {
+        if(exists){
+          var rndNow = Date.now();
+          outputFile = outputFile + '-' + rndNow;
+          fs.exists(outputFile + ".zip", callback);          
+        }
+         fs.writeFile(outputFile + ".zip", buffer, function(err) {
+           if (err) console.log(err);
+           cb(null,"/assets/downloads/" + path.basename(outputFile + ".zip"));
+         });
+      });
+      
     }
   ], function cb (err,result){
       console.log(result);
-      res.send({
-        url: result
-      });
+      req.filepath = result;
+      return next();     
   });
 
+}
 
-  // res.sendfile('meansible.zip');
 
-  // res.json({"message": "folder created"});
-  // var outputFile = '/assets/downloads/first.zip';
-  // res.send({
-  //   url: outputFile
-  // });
+// Generate and Send Scripts
+exports.getScript = function(req, res) {
+  console.log(req.filepath)
+  res.send({
+    url: req.filepath
+  });
 };
 
 
